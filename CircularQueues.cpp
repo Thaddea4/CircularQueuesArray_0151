01e815614541beaffb7eb00b4c694042631e0af4
Thaddea4 <adeyasyie@gmail.com>
2026-05-29 17:34:58 +0700

    mengatur posisi REAR pada circular queue

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -27,3 +27,16 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+        
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
